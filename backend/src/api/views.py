57d12502a4789158692376e0d82c8e43c64865b9
Felipe Oliveira <flipeaz342@gmail.com>
2025-02-23 20:07:53 -0400

    Utiliza self para acessar serializer e queryset em EventViewSet

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.serializers import EventSerializer
@@ -12,16 +12,18 @@ class EventViewSet(viewsets.ViewSet):
     queryset = Event.objects.all()
 
     def list(self, request):
-        queryset = Event.objects.all()
-        serializer = EventSerializer(queryset, many=True)
+        serializer = self.serializer_class(
+            self.queryset,
+            many=True,
+        )
         return Response(serializer.data)
 
     def create(self, request):
-        serializer = EventSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if not serializer.is_valid():
             return Response(
                 serializer.errors,
-                status=400,
+                status=status.HTTP_400_BAD_REQUESTs,
             )
         response = EventGlory().create_event(
             user=request.user,
