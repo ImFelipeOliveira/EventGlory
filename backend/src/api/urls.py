ff57f65e87ea7ddc941ea93bd9babcd42133d6df
Felipe Oliveira <flipeaz342@gmail.com>
2025-02-23 20:47:19 -0400

    add static settings

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from rest_framework import routers
+
+from eventglory import settings
 
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r"events", views.EventViewSet, basename="events")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
