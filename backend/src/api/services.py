6cffdd75fb148ce193546f97a608e9a2b3bc2904
Felipe Oliveira <flipeaz342@gmail.com>
2025-02-28 17:58:33 -0400

    Remove unused import

--- backend/src/api/services.py
+++ backend/src/api/services.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import transaction
-from rest_framework import status
-from rest_framework.response import Response
 
 from api.models import Endereco, Pessoa
 
