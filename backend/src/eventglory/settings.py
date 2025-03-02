c12c86135989f01650b2e932183b7feffce8047b
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-01 23:00:19 -0400

    Add django-cors-headers settings

--- backend/src/eventglory/settings.py
+++ backend/src/eventglory/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "oauth2_provider",
     "social_django",
     "drf_social_oauth2",
+    # CorsHeaders
+    "corsheaders",
     # APPs
     "api",
 ]
@@ -54,12 +56,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [getenv("CORS_ALLOWED_ORIGINS")]
 
 ROOT_URLCONF = "eventglory.urls"
 
