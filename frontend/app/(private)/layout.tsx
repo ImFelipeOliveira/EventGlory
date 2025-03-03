70882c1295350b9687458bb55a73eebdb85adb56
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-03 17:37:00 -0400

    Add layout private

---
+++ frontend/app/(private)/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/main-layout/header";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
