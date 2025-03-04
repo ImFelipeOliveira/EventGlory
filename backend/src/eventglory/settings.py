dc143a25ea2254ad947dec99c07a9560bfc910a9
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-04 01:09:52 -0400

    Adicionar configuração do SIMPLE_JWT para gerenciamento de tokens de autenticação

--- backend/src/eventglory/settings.py
+++ backend/src/eventglory/settings.py
@@ -9,6 +9,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from os import getenv
 from pathlib import Path
 
@@ -165,6 +166,22 @@ REST_FRAMEWORK = {
         "drf_social_oauth2.authentication.SocialAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken"),
+    "TOKEN_TYPE_CLAIM": "token_type",
 }
 
 SPECTACULAR_SETTINGS = {
