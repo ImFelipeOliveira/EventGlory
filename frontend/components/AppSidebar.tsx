631f2b6f66dbd738f92fd9cdf6308819d18e0eea
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-05 14:26:02 -0400

    Adiciona novos componentes de UI: Skeleton, Separator, Tooltip, Sheet e AppSidebar

---
+++ frontend/components/AppSidebar.tsx
@@ -0,0 +1,71 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="mt-16">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="scroll-m-20 text-xl font-semibold tracking-tight">
+            ÁREA DO CRIADOR
+          </SidebarGroupLabel>
+          <hr className="mb-2" />
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-1.5">
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
