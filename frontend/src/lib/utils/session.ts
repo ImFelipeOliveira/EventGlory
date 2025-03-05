5cb28d1f7f0de836d462e23210c13f1b9581d9d3
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-04 23:18:15 -0400

    Refatora função de decriptação de token para retornar null em caso de token vazio

--- frontend/src/lib/utils/session.ts
+++ frontend/src/lib/utils/session.ts
@@ -1,11 +1,10 @@
+import { Session } from "inspector/promises";
 import { decodeJwt } from "jose";
 
 export async function decrypt(token: string | undefined = "") {
-  try {
-    const tokenPayload = decodeJwt(token);
-    return tokenPayload;
-  } catch (error) {
-    console.error("Erro ao decodificar token: ", error);
-    return { error: error };
+  if (token === "") {
+    return null;
   }
+  const tokenPayload = decodeJwt(token);
+  return tokenPayload;
 }
